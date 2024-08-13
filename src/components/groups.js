0e4a7560c1bc16e4d76db39abe0fb141974e8194
KMJ <goodmj112@gmail.com>
2024-08-13 23:43:30 +0900

    Feat: 그룹 영역 생성 및  테스트 그룹정렬 시안 구현
    groups.js 생성 후,임의의 height를 가진 Test 그룹 div들로 테스트 그룹 정렬 시안 구현하였으며, MiddleGroups div에 SmallGroups div를 넣는 구조를 통해 더보기 버튼 클릭을 염두한 그룹정렬 방식 고안하였음.
     차후에 백엔드에서 그룹 불러오기  API가 구현될 경우 map 메소드를 통해 실질적인 그룹 정렬을 구현할 예정임.

---
+++ src/components/groups.js
@@ -0,0 +1,106 @@
+import styled from "styled-components";
+
+const GroupsOutter=styled.div`
+    display:flex;
+    width: 1560px;
+    margin: 60px 0 60px 0 ;
+    gap:20px;
+    flex-wrap:wrap;
+
+`
+const TestSmallGroup1=styled.div`
+    gap:20px;
+    width:375px;
+    height:406px;
+    background-color:red;
+`
+const TestSmallGroup2=styled.div`
+    gap:20px;
+    width:375px;
+    height:561px;
+    background-color:green;
+`
+const TestSmallGroup3=styled.div`
+    gap:20px;
+    width:375px;
+    height:206px;
+    background-color:blue;
+`
+const SmallGroups=styled.div`
+    display:flex;
+    gap:20px;
+    width:375px;
+    flex-direction:column;
+    flex-shrink:1;
+    flex-grow:0;
+
+`
+const MiddleGroups=styled.div`
+    display:flex;
+    gap:20px;
+    width:375px;
+    flex-direction:column;
+    flex-shrink:1;
+    flex-grow:0;
+
+`
+
+
+function Groups(){
+
+    return(
+
+        <GroupsOutter>
+            <MiddleGroups>
+                <SmallGroups>
+                        <TestSmallGroup1/>
+                        <TestSmallGroup1/>
+                        <TestSmallGroup3/>
+                </SmallGroups>
+                <SmallGroups>
+                        <TestSmallGroup2/>
+                        <TestSmallGroup1/>
+                        <TestSmallGroup1/>
+                </SmallGroups>
+            </MiddleGroups>
+            <MiddleGroups>
+                <SmallGroups>
+                        <TestSmallGroup2/>
+                        <TestSmallGroup1/>
+                        <TestSmallGroup3/>
+                </SmallGroups>
+                <SmallGroups>
+                        <TestSmallGroup1/>
+                        <TestSmallGroup2/>
+                        <TestSmallGroup1/>
+                </SmallGroups>
+            </MiddleGroups>
+            <MiddleGroups>
+                <SmallGroups>
+                        <TestSmallGroup3/>
+                        <TestSmallGroup2/>
+                        <TestSmallGroup3/>
+                </SmallGroups>
+                <SmallGroups>
+                        <TestSmallGroup2/>
+                        <TestSmallGroup3/>
+                        <TestSmallGroup1/>
+                </SmallGroups>
+            </MiddleGroups>
+            <MiddleGroups>
+                <SmallGroups>
+                        <TestSmallGroup1/>
+                        <TestSmallGroup2/>
+                        <TestSmallGroup3/>
+                </SmallGroups>
+                <SmallGroups>
+                        <TestSmallGroup3/>
+                        <TestSmallGroup2/>
+                        <TestSmallGroup1/>
+                </SmallGroups>
+            </MiddleGroups>
+        </GroupsOutter>
+    );
+}
+
+export default Groups;
